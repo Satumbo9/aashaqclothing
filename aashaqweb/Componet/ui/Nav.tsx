baa5162728db145f8518f6fae2aa48128edc5aa3
satumbo9 <hello@satumbo9.com>
2024-10-10 15:04:03 -0700

    BASE!!!

--- aashaqweb/Componet/ui/Nav.tsx
+++ aashaqweb/Componet/ui/Nav.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Nav() {
   return (
-    <header className="bg-red-500 h-screen w-40">
+    <header className="bg-red-500 h-screen w-10">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, id.
     </header>
   );
