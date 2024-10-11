9b378fe7b0fb9672e48c06ccd18b3999ff84c0ad
satumbo9 <hello@satumbo9.com>
2024-10-11 08:50:22 -0700

    Update

--- aashaqweb/app/layout.tsx
+++ aashaqweb/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-screen flex `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-screen xl:flex `}
       >
         <SideBar />
         {children}
